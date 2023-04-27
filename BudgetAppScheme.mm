51892a58b0d7b0117472017d1baa5b7afadbe46a
KarolKow294 <kowalski.karol294@gmail.com>
2023-04-28 00:28:07 +0200

    Creation of an application plan

---
+++ BudgetAppScheme.mm
@@ -0,0 +1,236 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1682452333426" ID="ID_1388076406" MODIFIED="1682452350279" TEXT="BudgetApp">
+<node CREATED="1682452390587" ID="ID_145039089" MODIFIED="1682452497261" POSITION="right" TEXT="Wy&#x15b;wietl Menu g&#x142;&#xf3;wne">
+<node CREATED="1682452565264" ID="ID_1683387951" MODIFIED="1682452601564" TEXT="Interfejs (czeka na znak)">
+<arrowlink DESTINATION="ID_1683387951" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1569567103" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1683387951" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1569567103" SOURCE="ID_1683387951" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<node CREATED="1682452605762" ID="ID_1398723162" MODIFIED="1682452624697" TEXT="Sprawdz czy podany znak jest znakiem pojedynczym">
+<node CREATED="1682452632205" ID="ID_1161895520" MODIFIED="1682452638665" TEXT="Tak">
+<node CREATED="1682452652110" ID="ID_1526700615" MODIFIED="1682452670318" TEXT="1. Rejestracja u&#x17c;ytkownika">
+<node CREATED="1682452908599" ID="ID_975571924" MODIFIED="1682452915940" TEXT="Podaj imie">
+<node CREATED="1682452918929" ID="ID_797734197" MODIFIED="1682452922976" TEXT="Podaj nazwisko">
+<node CREATED="1682452925665" ID="ID_1051566068" MODIFIED="1682541441679" TEXT="Podaj login">
+<node CREATED="1682541451146" ID="ID_334989007" MODIFIED="1682541465173" TEXT="Sprawdz czy login istnieje">
+<node CREATED="1682541470866" ID="ID_684022094" MODIFIED="1682541482925" TEXT="Tak">
+<node CREATED="1682541486428" ID="ID_817481370" MODIFIED="1682541520532" TEXT="Informacja: Login ju&#x17c; istnieje. Wprowad&#x17a; ponownie login.">
+<node CREATED="1682452963951" ID="ID_848592343" MODIFIED="1682452972925" TEXT="Podaj has&#x142;o">
+<node CREATED="1682541541995" ID="ID_1818945816" MODIFIED="1682541575226" TEXT="Informacja: U&#x17c;ytkownik zosta&#x142; zarejestrowany."/>
+</node>
+</node>
+</node>
+<node CREATED="1682541532771" ID="ID_1257882815" MODIFIED="1682541535352" TEXT="Nie">
+<node CREATED="1682452963951" ID="ID_637319221" MODIFIED="1682452972925" TEXT="Podaj has&#x142;o">
+<node CREATED="1682541541995" ID="ID_168463925" MODIFIED="1682541575226" TEXT="Informacja: U&#x17c;ytkownik zosta&#x142; zarejestrowany."/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1682452672921" ID="ID_423445852" MODIFIED="1682452684901" TEXT="2. Logowanie u&#x17c;ytkownika">
+<node CREATED="1682452984160" ID="ID_1754825187" MODIFIED="1682452988248" TEXT="Podaj login">
+<node CREATED="1682452991082" ID="ID_1492722594" MODIFIED="1682453059682" TEXT="Sprawdz czy podany login istnieje">
+<node CREATED="1682453068413" ID="ID_357340640" MODIFIED="1682453073972" TEXT="Tak">
+<node CREATED="1682453087742" ID="ID_1883308930" MODIFIED="1682453092501" TEXT="Podaj haslo">
+<node CREATED="1682453139636" ID="ID_761502156" MODIFIED="1682453156934" TEXT="Sprawdz czy has&#x142;o jest poprawne.">
+<node CREATED="1682453166114" ID="ID_1171764436" MODIFIED="1682453171920" TEXT="Tak">
+<node CREATED="1682453320727" ID="ID_550117264" MODIFIED="1682453339423" TEXT="Wyswietl Menu Zalogowanego Uzytkownika">
+<node CREATED="1682453487627" ID="ID_1488359806" MODIFIED="1682453498955" TEXT="Interfejs (czeka na znak)">
+<node CREATED="1682453508711" ID="ID_124065244" MODIFIED="1682453520078" TEXT="Sprawd&#x17a; czy podany znak jest znakiem pojedynczym.">
+<node CREATED="1682453587953" ID="ID_1786254465" MODIFIED="1682453598832" TEXT="1. Dodaj przych&#xf3;d">
+<node CREATED="1682453734276" ID="ID_803458732" MODIFIED="1682453763087" TEXT="Informacja: Czy przych&#xf3;d kt&#xf3;ry chcesz doda&#x107; dotyczy dnia dzisiejszego?">
+<node CREATED="1682453767929" ID="ID_585930133" MODIFIED="1682453770894" TEXT="Tak">
+<node CREATED="1682453813103" ID="ID_412150944" MODIFIED="1682453917578" TEXT="Pobierz dat&#x119; z systemu">
+<node CREATED="1682453928062" ID="ID_940767563" MODIFIED="1682454026991" TEXT="Podaj czego dotyczy przych&#xf3;d.">
+<node CREATED="1682453985684" ID="ID_863536426" MODIFIED="1682453997580" TEXT="Podaj wysoko&#x15b;&#x107; przychodzu.">
+<node CREATED="1682454514244" ID="ID_327513038" MODIFIED="1682454535380" TEXT="Sprawdz czy kwota jest liczb&#x105;.">
+<node CREATED="1682454545016" ID="ID_31784248" MODIFIED="1682454550342" TEXT="Tak">
+<node CREATED="1682456074976" ID="ID_1921225640" MODIFIED="1682456091258" TEXT="Informacja: Przych&#xf3;d zosta&#x142; dodany.">
+<node CREATED="1682454213204" ID="ID_1126146766" MODIFIED="1682454225594" TEXT="Wr&#xf3;&#x107; do Menu Zalogowanego U&#x17c;ytkownika."/>
+</node>
+</node>
+<node CREATED="1682454557759" ID="ID_1149055255" MODIFIED="1682454563536" TEXT="Nie">
+<node CREATED="1682454585988" ID="ID_1340893320" MODIFIED="1682454607895" TEXT="Informacja: Wprowadzony przych&#xf3;d nie jest liczb&#x105;. Wprowad&#x17a; ponownie.">
+<node CREATED="1682454690361" ID="ID_427571625" MODIFIED="1682455914146" TEXT="Po wprowadzeniu kwoty w odpowiednim formacie poinformuj: Przych&#xf3;d zosta&#x142; dodany.">
+<node CREATED="1682455916932" ID="ID_1003983150" MODIFIED="1682455930606" TEXT="Wr&#xf3;&#x107; do Menu Zalogowanego U&#x17c;ytkownika"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1682453774552" ID="ID_752111479" MODIFIED="1682453776817" TEXT="Nie">
+<node CREATED="1682454086282" ID="ID_1284270665" MODIFIED="1682454111628" TEXT="Podaj date w formie rrrr-mm-dd">
+<node CREATED="1682454122527" ID="ID_839651768" MODIFIED="1682454165514" TEXT="Sprawd&#x17a; czy data mie&#x15b;ci si&#x119; od 2000-01-01 do ostatniego dnia bie&#x17c;&#x105;cego miesi&#x105;ca.">
+<node CREATED="1682625416441" ID="ID_477817371" MODIFIED="1682625424418" TEXT="Tak">
+<node CREATED="1682454189416" ID="ID_1800775796" MODIFIED="1682454198103" TEXT="Podaj czego dotyczy przych&#xf3;d.">
+<node CREATED="1682454200497" ID="ID_1123438750" MODIFIED="1682454209632" TEXT="Podaj wysoko&#x15b;&#x107; przychodu.">
+<node CREATED="1682454514244" ID="ID_699880175" MODIFIED="1682454535380" TEXT="Sprawdz czy kwota jest liczb&#x105;.">
+<node CREATED="1682454545016" ID="ID_337465902" MODIFIED="1682454550342" TEXT="Tak">
+<node CREATED="1682454213204" ID="ID_1680249499" MODIFIED="1682454225594" TEXT="Wr&#xf3;&#x107; do Menu Zalogowanego U&#x17c;ytkownika."/>
+</node>
+<node CREATED="1682454557759" ID="ID_571856920" MODIFIED="1682454563536" TEXT="Nie">
+<node CREATED="1682454585988" ID="ID_841129285" MODIFIED="1682454607895" TEXT="Informacja: Wprowadzony przych&#xf3;d nie jest liczb&#x105;. Wprowad&#x17a; ponownie.">
+<node CREATED="1682454690361" ID="ID_1320284163" MODIFIED="1682455914146" TEXT="Po wprowadzeniu kwoty w odpowiednim formacie poinformuj: Przych&#xf3;d zosta&#x142; dodany.">
+<node CREATED="1682455916932" ID="ID_701145838" MODIFIED="1682455930606" TEXT="Wr&#xf3;&#x107; do Menu Zalogowanego U&#x17c;ytkownika"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1682625429316" ID="ID_1832500244" MODIFIED="1682625432562" TEXT="Nie">
+<node CREATED="1682625448304" ID="ID_304132767" MODIFIED="1682625489716" TEXT="Informacja: Wprowadzona data nie mie&#x15b;ci si&#x119; w zakresie. Wprowad&#x17a; ponownie now&#x105; dat&#x119;.">
+<node CREATED="1682454189416" ID="ID_12564697" MODIFIED="1682454198103" TEXT="Podaj czego dotyczy przych&#xf3;d.">
+<node CREATED="1682454200497" ID="ID_902543023" MODIFIED="1682454209632" TEXT="Podaj wysoko&#x15b;&#x107; przychodu.">
+<node CREATED="1682454514244" ID="ID_1699878879" MODIFIED="1682454535380" TEXT="Sprawdz czy kwota jest liczb&#x105;.">
+<node CREATED="1682454545016" ID="ID_681182366" MODIFIED="1682454550342" TEXT="Tak">
+<node CREATED="1682454213204" ID="ID_1418978281" MODIFIED="1682454225594" TEXT="Wr&#xf3;&#x107; do Menu Zalogowanego U&#x17c;ytkownika."/>
+</node>
+<node CREATED="1682454557759" ID="ID_467963854" MODIFIED="1682454563536" TEXT="Nie">
+<node CREATED="1682454585988" ID="ID_1232163538" MODIFIED="1682454607895" TEXT="Informacja: Wprowadzony przych&#xf3;d nie jest liczb&#x105;. Wprowad&#x17a; ponownie.">
+<node CREATED="1682454690361" ID="ID_888783714" MODIFIED="1682455914146" TEXT="Po wprowadzeniu kwoty w odpowiednim formacie poinformuj: Przych&#xf3;d zosta&#x142; dodany.">
+<node CREATED="1682455916932" ID="ID_1589297375" MODIFIED="1682455930606" TEXT="Wr&#xf3;&#x107; do Menu Zalogowanego U&#x17c;ytkownika"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1682453601346" ID="ID_1187891253" MODIFIED="1682453609040" TEXT="2. Dodaj wydatek">
+<node CREATED="1682454278380" ID="ID_840481011" MODIFIED="1682454297684" TEXT="Informacja: Czy wydatek kt&#xf3;ry chcesz doda&#x107; dotyczy dnia dzisiejszego?">
+<node CREATED="1682454300157" ID="ID_745098430" MODIFIED="1682454302187" TEXT="Tak">
+<node CREATED="1682453813103" ID="ID_729923924" MODIFIED="1682453917578" TEXT="Pobierz dat&#x119; z systemu">
+<node CREATED="1682453928062" ID="ID_760066723" MODIFIED="1682454349180" TEXT="Podaj czego dotyczy wydatek.">
+<node CREATED="1682453985684" ID="ID_1404532387" MODIFIED="1682454358995" TEXT="Podaj wysoko&#x15b;&#x107; wydatku.">
+<node CREATED="1682454514244" ID="ID_1825936871" MODIFIED="1682454535380" TEXT="Sprawdz czy kwota jest liczb&#x105;.">
+<node CREATED="1682454545016" ID="ID_945887284" MODIFIED="1682454550342" TEXT="Tak">
+<node CREATED="1682456074976" ID="ID_477167415" MODIFIED="1682456091258" TEXT="Informacja: Przych&#xf3;d zosta&#x142; dodany.">
+<node CREATED="1682454213204" ID="ID_1140576356" MODIFIED="1682454225594" TEXT="Wr&#xf3;&#x107; do Menu Zalogowanego U&#x17c;ytkownika."/>
+</node>
+</node>
+<node CREATED="1682454557759" ID="ID_1158577034" MODIFIED="1682454563536" TEXT="Nie">
+<node CREATED="1682454585988" ID="ID_1238342414" MODIFIED="1682456013479" TEXT="Informacja: Wprowadzony wydatek nie jest liczb&#x105;. Wprowad&#x17a; ponownie.">
+<node CREATED="1682454690361" ID="ID_1360458134" MODIFIED="1682456023378" TEXT="Po wprowadzeniu kwoty w odpowiednim formacie poinformuj: Wydatek zosta&#x142; dodany.">
+<node CREATED="1682455916932" ID="ID_1778426704" MODIFIED="1682455930606" TEXT="Wr&#xf3;&#x107; do Menu Zalogowanego U&#x17c;ytkownika"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1682454304736" ID="ID_1471502368" MODIFIED="1682454307190" TEXT="Nie">
+<node CREATED="1682454086282" ID="ID_153980002" MODIFIED="1682454111628" TEXT="Podaj date w formie rrrr-mm-dd">
+<node CREATED="1682454122527" ID="ID_1847017884" MODIFIED="1682454165514" TEXT="Sprawd&#x17a; czy data mie&#x15b;ci si&#x119; od 2000-01-01 do ostatniego dnia bie&#x17c;&#x105;cego miesi&#x105;ca.">
+<node CREATED="1682625530035" ID="ID_804509829" MODIFIED="1682625533552" TEXT="Tak">
+<node CREATED="1682454189416" ID="ID_1070997499" MODIFIED="1682454403564" TEXT="Podaj czego dotyczy wydatek.">
+<node CREATED="1682454200497" ID="ID_1973826983" MODIFIED="1682454410720" TEXT="Podaj wysoko&#x15b;&#x107; wydatku.">
+<node CREATED="1682454514244" ID="ID_1348489399" MODIFIED="1682454535380" TEXT="Sprawdz czy kwota jest liczb&#x105;.">
+<node CREATED="1682454545016" ID="ID_816557623" MODIFIED="1682454550342" TEXT="Tak">
+<node CREATED="1682454213204" ID="ID_1569225877" MODIFIED="1682454225594" TEXT="Wr&#xf3;&#x107; do Menu Zalogowanego U&#x17c;ytkownika."/>
+</node>
+<node CREATED="1682454557759" ID="ID_1291860213" MODIFIED="1682454563536" TEXT="Nie">
+<node CREATED="1682454585988" ID="ID_1845904497" MODIFIED="1682456041267" TEXT="Informacja: Wprowadzony wydatek nie jest liczb&#x105;. Wprowad&#x17a; ponownie.">
+<node CREATED="1682454690361" ID="ID_1339917104" MODIFIED="1682456048211" TEXT="Po wprowadzeniu kwoty w odpowiednim formacie poinformuj: Wydatek zosta&#x142; dodany.">
+<node CREATED="1682455916932" ID="ID_233485752" MODIFIED="1682455930606" TEXT="Wr&#xf3;&#x107; do Menu Zalogowanego U&#x17c;ytkownika"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1682625538661" ID="ID_1008843726" MODIFIED="1682625541162" TEXT="Nie">
+<node CREATED="1682625448304" ID="ID_388385375" MODIFIED="1682625489716" TEXT="Informacja: Wprowadzona data nie mie&#x15b;ci si&#x119; w zakresie. Wprowad&#x17a; ponownie now&#x105; dat&#x119;.">
+<node CREATED="1682454189416" ID="ID_1098211856" MODIFIED="1682454198103" TEXT="Podaj czego dotyczy przych&#xf3;d.">
+<node CREATED="1682454200497" ID="ID_1063402028" MODIFIED="1682454209632" TEXT="Podaj wysoko&#x15b;&#x107; przychodu.">
+<node CREATED="1682454514244" ID="ID_754227141" MODIFIED="1682454535380" TEXT="Sprawdz czy kwota jest liczb&#x105;.">
+<node CREATED="1682454545016" ID="ID_1999228472" MODIFIED="1682454550342" TEXT="Tak">
+<node CREATED="1682454213204" ID="ID_1899774813" MODIFIED="1682454225594" TEXT="Wr&#xf3;&#x107; do Menu Zalogowanego U&#x17c;ytkownika."/>
+</node>
+<node CREATED="1682454557759" ID="ID_1463621439" MODIFIED="1682454563536" TEXT="Nie">
+<node CREATED="1682454585988" ID="ID_942761049" MODIFIED="1682454607895" TEXT="Informacja: Wprowadzony przych&#xf3;d nie jest liczb&#x105;. Wprowad&#x17a; ponownie.">
+<node CREATED="1682454690361" ID="ID_530679090" MODIFIED="1682455914146" TEXT="Po wprowadzeniu kwoty w odpowiednim formacie poinformuj: Przych&#xf3;d zosta&#x142; dodany.">
+<node CREATED="1682455916932" ID="ID_1285640533" MODIFIED="1682455930606" TEXT="Wr&#xf3;&#x107; do Menu Zalogowanego U&#x17c;ytkownika"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1682453612596" ID="ID_1092335262" MODIFIED="1682453622658" TEXT="3. Bilans z bie&#x17c;&#x105;cego miesi&#x105;ca">
+<node CREATED="1682454841229" ID="ID_1633367007" MODIFIED="1682455511937" TEXT="Wy&#x15b;wietl posegregowane przychodzy a nastepnie wydatki. Od najstarszego do najmlodszego. Na ko&#x144;cu wy&#x15b;wietl sum&#x119; przychod&#xf3;w oraz wydatk&#xf3;w. Dodatkowo wy&#x15b;wietl r&#xf3;&#x17c;nic&#x119; mi&#x119;dzy nimi.">
+<node CREATED="1682456150394" ID="ID_1694348937" MODIFIED="1682456171648" TEXT="Po naci&#x15b;ni&#x119;ciu dowolnego klawisza przechodzi do Menu Zalogowanego U&#x17c;ytkownika"/>
+</node>
+</node>
+<node CREATED="1682453626511" ID="ID_1660931515" MODIFIED="1682453638188" TEXT="4. Bilans z poprzedniego miesi&#x105;ca">
+<node CREATED="1682454841229" ID="ID_1988190321" MODIFIED="1682455511937" TEXT="Wy&#x15b;wietl posegregowane przychodzy a nastepnie wydatki. Od najstarszego do najmlodszego. Na ko&#x144;cu wy&#x15b;wietl sum&#x119; przychod&#xf3;w oraz wydatk&#xf3;w. Dodatkowo wy&#x15b;wietl r&#xf3;&#x17c;nic&#x119; mi&#x119;dzy nimi.">
+<node CREATED="1682456150394" ID="ID_831653296" MODIFIED="1682456171648" TEXT="Po naci&#x15b;ni&#x119;ciu dowolnego klawisza przechodzi do Menu Zalogowanego U&#x17c;ytkownika"/>
+</node>
+</node>
+<node CREATED="1682453653145" ID="ID_754724617" MODIFIED="1682453672626" TEXT="5. Bilans z wybranego okresu">
+<node CREATED="1682455566846" ID="ID_325227031" MODIFIED="1682455683573" TEXT="Podaj od jakiego okresu wyswietlic bilans przychod&#xf3;w i wydatk&#xf3;w w formie yyyy-mm-dd">
+<node CREATED="1682455640448" ID="ID_1566385235" MODIFIED="1682455673716" TEXT="Podaj do jakiego okresu wy&#x15b;wietli&#x107; bilans przychod&#xf3;w i wydatk&#xf3;w w formie yyyy-mm-dd">
+<node CREATED="1682454841229" ID="ID_888860129" MODIFIED="1682455511937" TEXT="Wy&#x15b;wietl posegregowane przychodzy a nastepnie wydatki. Od najstarszego do najmlodszego. Na ko&#x144;cu wy&#x15b;wietl sum&#x119; przychod&#xf3;w oraz wydatk&#xf3;w. Dodatkowo wy&#x15b;wietl r&#xf3;&#x17c;nic&#x119; mi&#x119;dzy nimi.">
+<node CREATED="1682456150394" ID="ID_489621959" MODIFIED="1682456171648" TEXT="Po naci&#x15b;ni&#x119;ciu dowolnego klawisza przechodzi do Menu Zalogowanego U&#x17c;ytkownika"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1682453679980" ID="ID_1653418426" MODIFIED="1682453688612" TEXT="6. Zmie&#x144; has&#x142;o">
+<node CREATED="1682455767834" ID="ID_1630049557" MODIFIED="1682455773424" TEXT="Podaj nowe haslo">
+<node CREATED="1682455777772" ID="ID_490496909" MODIFIED="1682455801716" TEXT="Informacja: Haslo zostalo zmienione">
+<node CREATED="1682455812520" ID="ID_1427239069" MODIFIED="1682455824969" TEXT="Wr&#xf3;&#x107; do Menu Zalogowanego U&#x17c;ytkownika"/>
+</node>
+</node>
+</node>
+<node CREATED="1682453691489" ID="ID_1248309239" MODIFIED="1682453700782" TEXT="7. Wyloguj si&#x119;">
+<node CREATED="1682455746455" ID="ID_643961577" MODIFIED="1682455758566" TEXT="Wr&#xf3;&#x107; do Menu G&#x142;&#xf3;wnego"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1682453184362" ID="ID_407401182" MODIFIED="1682453186961" TEXT="Nie">
+<node CREATED="1682453189266" ID="ID_1935189131" MODIFIED="1682453207246" TEXT="Po trzech pr&#xf3;bach wr&#xf3;c do Menu G&#x142;&#xf3;wnego."/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1682453076861" ID="ID_1418403092" MODIFIED="1682453078955" TEXT="Nie">
+<node CREATED="1682453102801" ID="ID_1920476246" MODIFIED="1682453135129" TEXT="Informacja: U&#x17c;ytkownik o tym loginie nie isnieje. Wpisz ponownie."/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1682452705192" ID="ID_75689101" MODIFIED="1682452714038" TEXT="8. Zako&#x144;cz program"/>
+</node>
+<node CREATED="1682452718717" ID="ID_1843386738" MODIFIED="1682452721486" TEXT="Nie">
+<node CREATED="1682452736331" ID="ID_615409957" MODIFIED="1682452772493" TEXT="Informacja: To nie jest pojedynczy znak. Wpisz ponownie."/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
